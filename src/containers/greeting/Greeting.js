ec9f6196801c92d8e680eb7d883942349cfe92a7
ehigie aito <ehigie.aito@platform.sh>
2020-07-25 06:05:15 +0100

    updates

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -28,7 +28,7 @@ export default function Greeting() {
           </div>
         </div>
         <div className="greeting-image-div">
-          <img alt="saad sitting on table" src={require("../../assets/images/manOnTable.svg")}></img>
+          <img alt="Portfolio Owner Image" src={require("../../assets/images/man.png")}></img>
         </div>
       </div>
     </div>
